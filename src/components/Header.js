c2fb82abaf1137507482dfbd8d59ccda4ed6f4d8
Aayush Regmi <regmiaayush7@gmail.com>
2021-10-06 23:20:08 +0545

    simple blogpost for react-redux refreshment

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+        <nav className='navbar navbar-dark bg-primary mb-3'>
+            <div className="container">
+                <a href="#" className="navbar-brand">BlogPost</a>
+            </div>
+        </nav>
+        </>
+    )
+}
+
+export default Header
